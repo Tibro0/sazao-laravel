14c403d17773b71fa7ac1ce614d0cd000251dd91
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-10-20 21:05:05 +0600

    Frontend Template Mastering

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(HomeController::class)->group(function () {
+    Route::get('/', 'index')->name('home');
 });
 
 Route::get('/dashboard', function () {
