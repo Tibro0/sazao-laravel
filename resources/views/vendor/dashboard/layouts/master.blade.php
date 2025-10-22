dd0bbece04f949c5abff37515676c4319ce26d9a
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-10-22 20:43:24 +0600

    Some Update

--- resources/views/vendor/dashboard/layouts/master.blade.php
+++ resources/views/vendor/dashboard/layouts/master.blade.php
@@ -35,7 +35,7 @@
   <!--=============================DASHBOARD MENU START==============================-->
   <div class="wsus__dashboard_menu">
     <div class="wsusd__dashboard_user">
-      <img src="="{{ asset(auth()->user()->image) }}" alt="img" class="img-fluid">
+      <img src="{{ asset(auth()->user()->image) }}" alt="img" class="img-fluid">
       <p>{{ auth()->user()->name }}</p>
     </div>
   </div>
