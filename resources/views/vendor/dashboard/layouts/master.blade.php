143ffd5c635d167793a4f10488d224d3f2fd67e1
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-10-22 20:39:43 +0600

    Vender & User Dashboard Information Display

--- resources/views/vendor/dashboard/layouts/master.blade.php
+++ resources/views/vendor/dashboard/layouts/master.blade.php
@@ -35,8 +35,8 @@
   <!--=============================DASHBOARD MENU START==============================-->
   <div class="wsus__dashboard_menu">
     <div class="wsusd__dashboard_user">
-      <img src="images/dashboard_user.jpg" alt="img" class="img-fluid">
-      <p>anik roy</p>
+      <img src="="{{ asset(auth()->user()->image) }}" alt="img" class="img-fluid">
+      <p>{{ auth()->user()->name }}</p>
     </div>
   </div>
   <!--=============================DASHBOARD MENU END==============================-->
