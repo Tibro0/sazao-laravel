9d120fadbf247dd31206a3baf799780adeceb942
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-10-25 19:42:45 +0600

    Add Brand Seeder Backend

--- database/seeders/BrandSeeder.php
+++ database/seeders/BrandSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Brand;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class BrandSeeder extends Seeder
 {
@@ -12,6 +14,70 @@ class BrandSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Brand::insert([
+            [
+                'logo' => 'frontend/images/main-image/brand/apple.png',
+                'name' => 'Apple',
+                'slug' => Str::slug('Apple'),
+                'is_featured' => 1,
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'logo' => 'frontend/images/main-image/brand/google.png',
+                'name' => 'Google',
+                'slug' => Str::slug('Google'),
+                'is_featured' => 1,
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'logo' => 'frontend/images/main-image/brand/JBL.png',
+                'name' => 'JBL',
+                'slug' => Str::slug('JBL'),
+                'is_featured' => 1,
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'logo' => 'frontend/images/main-image/brand/onePlus.png',
+                'name' => 'OnePlus',
+                'slug' => Str::slug('OnePlus'),
+                'is_featured' => 1,
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'logo' => 'frontend/images/main-image/brand/oppo.png',
+                'name' => 'Oppo',
+                'slug' => Str::slug('Oppo'),
+                'is_featured' => 1,
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'logo' => 'frontend/images/main-image/brand/realme.jpg',
+                'name' => 'Realme',
+                'slug' => Str::slug('Realme'),
+                'is_featured' => 1,
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'logo' => 'frontend/images/main-image/brand/samsung.png',
+                'name' => 'Samsung',
+                'slug' => Str::slug('Samsung'),
+                'is_featured' => 1,
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
