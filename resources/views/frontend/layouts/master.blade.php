f6e1b2ebd96cfe0aea1ad031fe54d6a1c708a901
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-11-19 19:17:21 +0600

    Frontend Popular Category Section

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -144,6 +144,11 @@
             }
         });
     </script>
+    <script>
+        $(document).ready(function() {
+            $('.auto_click').click();
+        });
+    </script>
     @include('frontend.layouts.scripts')
     @yield('js-link')
 </body>
