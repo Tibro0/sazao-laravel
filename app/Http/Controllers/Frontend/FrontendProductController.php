af6bd585452579f436b4e00e654440290d23a331
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-11-19 23:51:18 +0600

    Some Update

--- app/Http/Controllers/Frontend/FrontendProductController.php
+++ app/Http/Controllers/Frontend/FrontendProductController.php
@@ -13,7 +13,7 @@ class FrontendProductController extends Controller
     public function showProduct(string $slug)
     {
         $flashSaleDate = FlashSale::first();
-        $product = Product::with(['vendor', 'category', 'productImageGalleries', 'variants', 'brand'])->where(['slug' => $slug, 'status' => 1])->first();
+        $product = Product::with(['vendor', 'category', 'productImageGalleries', 'variants', 'brand'])->where(['slug' => $slug, 'status' => 1, 'is_approved' => 1])->first();
         return view('frontend.pages.product-detail', compact('product', 'flashSaleDate'));
     }
 }
