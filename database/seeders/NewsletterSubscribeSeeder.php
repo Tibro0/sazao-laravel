0d4b59828a27319f49f4a442ed5a3f46c33ef986
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-11-26 17:13:58 +0600

    News Letter Verification Seeder Add

--- database/seeders/NewsletterSubscribeSeeder.php
+++ database/seeders/NewsletterSubscribeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\NewsletterSubscribe;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,28 @@ class NewsletterSubscribeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        NewsletterSubscribe::insert([
+            [
+                'email' => 'admin@gmail.com',
+                'verified_token' => 'verified',
+                'is_verified' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'email' => 'vendor@gmail.com',
+                'verified_token' => 'verified',
+                'is_verified' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'email' => 'user@gmail.com',
+                'verified_token' => 'verified',
+                'is_verified' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
