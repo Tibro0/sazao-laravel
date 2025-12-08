11c14a8628b541f6af78ce8cbf9ef54e56c5cf4f
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-12-08 15:38:42 +0600

    Blog Category Seeder Add

--- database/seeders/BlogCategorySeeder.php
+++ database/seeders/BlogCategorySeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\BlogCategory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class BlogCategorySeeder extends Seeder
 {
@@ -12,6 +14,49 @@ class BlogCategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        BlogCategory::insert([
+            [
+                'name' => 'Mobile',
+                'slug' => Str::slug('Mobile'),
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Computer',
+                'slug' => Str::slug('Computer'),
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Fitness',
+                'slug' => Str::slug('Fitness'),
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Apple',
+                'slug' => Str::slug('Apple'),
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Samsung',
+                'slug' => Str::slug('Samsung'),
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Laptop',
+                'slug' => Str::slug('Laptop'),
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
