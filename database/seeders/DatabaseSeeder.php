88ddac993b5defa5fe8a4c8c3aeae99ab03637c1
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2026-02-18 21:47:44 +0600

    Google Seeder Update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -69,6 +69,8 @@ class DatabaseSeeder extends Seeder
             WithdrawRequestSeeder::class,
 
             LogoSettingSeeder::class,
+
+            GoogleSettingSeeder::class,
         ]);
     }
 }
--- database/seeders/GoogleSettingSeeder.php
+++ database/seeders/GoogleSettingSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\GoogleSetting;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,14 @@ class GoogleSettingSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        GoogleSetting::insert([
+            [
+                'google_client_id' => 'Google Client ID',
+                'google_client_secret' => 'Google Client Secret',
+                'google_redirect_url' => 'http://127.0.0.1:8000/auth/google-callback',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
     }
 }
